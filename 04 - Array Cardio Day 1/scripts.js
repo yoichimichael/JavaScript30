b563e6e096be631971c561e8dc88fd717f4ba8ab
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-09-26 23:53:53 -0400

    first two exercises completed

--- 04 - Array Cardio Day 1/scripts.js
+++ 04 - Array Cardio Day 1/scripts.js
@@ -23,8 +23,23 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 
+// const fifteen = inventors.filter(function(inventor) {
+//   if(inventor.year >= 1500 && inventor.year < 1600) {
+//     return true; // keep object!
+//   }
+// });
+
+// refactor
+// single line with arrow function has implicit return
+const fifteenHundreds = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+
+// console.table(fifteenHundreds)
+
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+
+const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`)
+console.table(fullNames)
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
