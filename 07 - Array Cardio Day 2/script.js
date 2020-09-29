0624834c9af0751eaa662b26d02fffdd4c8ac4f4
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-09-29 11:53:23 -0400

    added solutions to exerises 1 and 2

--- 07 - Array Cardio Day 2/script.js
+++ 07 - Array Cardio Day 2/script.js
@@ -1,28 +1,63 @@
-  // ## Array Cardio Day 2
+// ## Array Cardio Day 2
 
-  const people = [
-    { name: 'Wes', year: 1988 },
-    { name: 'Kait', year: 1986 },
-    { name: 'Irv', year: 1970 },
-    { name: 'Lux', year: 2015 }
-  ];
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
 
-  const comments = [
-    { text: 'Love this!', id: 523423 },
-    { text: 'Super good', id: 823423 },
-    { text: 'You are the best', id: 2039842 },
-    { text: 'Ramen is my fav food ever', id: 123523 },
-    { text: 'Nice Nice Nice!', id: 542328 }
-  ];
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
 
-  // Some and Every Checks
-  // Array.prototype.some() // is at least one person 19 or older?
-  // Array.prototype.every() // is everyone 19 or older?
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
 
-  // Array.prototype.find()
-  // Find is like filter, but instead returns just the one you are looking for
-  // find the comment with the ID of 823423
+// const isAdult = people.some(function(person) {
+//   const currentYear = (new Date()).getFullYear();
+//   if (currentYear - person.year >= 19) {
+//     return true;
+//   }
+// });
 
-  // Array.prototype.findIndex()
-  // Find the comment with this ID
-  // delete the comment with the ID of 823423
+// refactor
+const isAdult = people.some(person => {
+  const currentYear = (new Date()).getFullYear();
+  return currentYear - person.year >= 19;
+});
+
+// adding curly braces will show name of variable and return value in console
+// console.log({isAdult})
+
+// Array.prototype.every() // is everyone 19 or older?
+
+const areAllAdults = people.every(person => {
+  const currentYear = (new Date()).getFullYear();
+  return currentYear - person.year >= 19;
+});
+
+console.log(areAllAdults)
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+// const matchedComment = comments.find(function(comment) {
+//   if(comment.id === 823423) {
+//     return true;
+//   }
+// })
+
+// refactor
+const matchedComment = comments.find(comment => comment.id === 823423);
+
+console.log(matchedId)
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
