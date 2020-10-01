aa917b1d766fdc417760bb744818dfa2e54a9d0f
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-09-30 23:31:17 -0400

    modified draw logic, simple canvas draw now functional

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -17,10 +17,31 @@ let lastY = 0;
 
 function draw(e) {
   if(!isDrawing) return; // will stop function from running if not mousedown
+
+  // below code will only run on mousedown
   console.log(e)
+  ctx.beginPath();
+
+  // starts from
+  ctx.moveTo(lastX, lastY);
+
+  // goes to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+
+
+  // lastX = e.offsetX;
+  // lastY = e.offsetY;
+  // fancy ES6 syntax for above; 'destructuring an array'
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
 // function inside draw will only run on mousedown
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  // starts drawing at mousedown, not [x=0, y=0]
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
