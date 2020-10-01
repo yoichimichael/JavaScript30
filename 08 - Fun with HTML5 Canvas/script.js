9418c2acf08e0c9be7ec114829234962e1d44c1a
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-09-30 23:08:11 -0400

    added brush behavior

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -5,3 +5,12 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// set the way the brush behaves
+ctx.strokeStyle = '#BADA55';
+ctx.linJoin = 'round';
+ctx.linCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
