062de287c382294f7f53d621ca2a33c3b8682602
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-01 00:05:31 -0400

    added line width logic

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -16,12 +16,13 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0; 
 let hue = 0;
+let direction = true;
 
 function draw(e) {
   if(!isDrawing) return; // will stop function from running if not mousedown
 
   // below code will only run on mousedown
-  console.log(e)
+  // console.log(e)
                   // hue, saturation, lightness  
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
   ctx.beginPath();
@@ -40,6 +41,20 @@ function draw(e) {
 
   // increments hsl hue
   hue++
+  // below resets hue value, but is unecessary; 360 === 0; 361 === 1; etc
+  if(hue >= 360) {
+    hue = 0;
+  }
+  
+  // lineWidth increases and decreases between 0 and 100
+  if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1){
+    direction = !direction; 
+  }
+  if(direction){
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
 }
 
 // function inside draw will only run on mousedown
