1576745d637764e817c23e433a8211a7317d648d
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-01 23:21:50 -0400

    moved/modified click event from inline to separate file

---
+++ 09 - Dev Tools Domination/script.js
@@ -0,0 +1,34 @@
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+
+function makeGreen(e) {
+  // const p = document.querySelector('p');
+  e.target.style.color = '#BADA55';
+  e.target.style.fontSize = '50px';
+}
+
+const p = document.querySelector('p');
+p.addEventListener('click', makeGreen)
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
