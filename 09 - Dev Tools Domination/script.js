2f9ef0879e8a7d4a58094c905b3ccdb49c6c9ba8
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-01 23:57:28 -0400

    added console examples

--- 09 - Dev Tools Domination/script.js
+++ 09 - Dev Tools Domination/script.js
@@ -10,24 +10,43 @@ const p = document.querySelector('p');
 p.addEventListener('click', makeGreen)
 
 // Regular
+console.log('hello');
 
 // Interpolated
+console.log('hello I am a %s string!', '💩')
+
+let poop = '💩'
+console.log(`hello I am a ${poop} string`)
 
 // Styled
+console.log('%c I am some great text', 'font-size: 20px; background: red; text-shadow: 5px 5px 0 blue')
 
 // warning!
+// includes stack trace
+console.warn("Attention please!")
 
 // Error :|
+// includes stack trace
+console.error("Something's definitely wrong!")
 
 // Info
+console.info('crocodiles eat 3-4 people a year!')
 
 // Testing
+// only displays second argument if first argument is false
+console.assert(1 === 2, "That's just not true!")
 
 // clearing
+// console.clear();
 
 // Viewing DOM Elements
+// shows all the different properties and methods
+console.dir(p)
 
 // Grouping together
+dogs.forEach(dog => {
+  console.log(`This is ${dog.name}`)
+});
 
 // counting
 
