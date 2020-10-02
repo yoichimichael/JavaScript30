09b9c394b3e52efcc201107f54b74d07f419ce12
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-02 00:08:19 -0400

    finished exercise

--- 09 - Dev Tools Domination/script.js
+++ 09 - Dev Tools Domination/script.js
@@ -44,10 +44,33 @@ console.assert(1 === 2, "That's just not true!")
 console.dir(p)
 
 // Grouping together
+// Groups lots of related logs together
 dogs.forEach(dog => {
-  console.log(`This is ${dog.name}`)
+  console.group(`${dog.name}`); // can also use console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog-years old`);
+  console.groupEnd(`${dog.name}`);
 });
 
 // counting
+console.count('Yay!');
+console.count('Yay!');
+console.count('Yay!');
+console.count('Boo!');
+console.count('Yay!');
+console.count('Boo!');
+console.count('Yay!');
+
+console.clear();
 
 // timing
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd('fetching data');
+    console.log(data);
+  });
+
+console.table(dogs)
