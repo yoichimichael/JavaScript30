e67243bf6b85e28b634dd03c16575d06c3aad955
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-08 23:09:49 -0400

    last commit

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -8,13 +8,12 @@ function handleCheck(e) {
 	console.log(e)
 	let inBetween = false;
 	if(e.shiftKey && this.checked) {
-		// go ahead and do what we please
 		// loop over every single checkbox
 		checkboxes.forEach(checkbox => {
-			// console.log(checkbox)
+			console.log(checkbox)
 			if(checkbox === this || checkbox === lastChecked) {
 
-				// toggles
+				// toggles inBetween
 				inBetween = !inBetween;
 				// console.log("starting to check for inBetween")
 			}
@@ -30,3 +29,13 @@ function handleCheck(e) {
 // click events will fire even when if use your keyboard 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
 
+/*
+	1. assign checkboxes into variable
+	2. add event listeners to checkboxes
+	3. declare lastChecked variable, but don't assign a value
+	4. declare and write handleCheck method. This method will:
+		- assign a value of 'this' to lastChecked
+		- if the the shift key is depressed while the box is checked, a loop over each box will be started
+			- if the box is between a previously checked box AND the current box being checked, those boxes will also be checked
+		a. declare a toggle variable to determine wether
+*/
