8d21f978abcc6ca8e4c93216f1b12d7183ea2972
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-08 23:09:23 -0400

    finish exercise

---
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -0,0 +1,32 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+
+let lastChecked;
+
+function handleCheck(e) {
+	// Check if they had the shift key down
+	// AND check that they are checking it
+	console.log(e)
+	let inBetween = false;
+	if(e.shiftKey && this.checked) {
+		// go ahead and do what we please
+		// loop over every single checkbox
+		checkboxes.forEach(checkbox => {
+			// console.log(checkbox)
+			if(checkbox === this || checkbox === lastChecked) {
+
+				// toggles
+				inBetween = !inBetween;
+				// console.log("starting to check for inBetween")
+			}
+
+			if(inBetween) {
+				checkbox.checked = true;
+			}
+		})
+	}
+	lastChecked = this;
+}
+
+// click events will fire even when if use your keyboard 
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
