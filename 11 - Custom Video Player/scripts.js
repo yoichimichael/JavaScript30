e54216598ac130894d3178b755e2094ea0f24a2c
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-12 13:08:31 -0400

    finish skip functions

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,64 @@
+/* ------------------------
+  Get our elements
+/* ------------------------
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* ------------------------
+  Build Our Functions
+/* ------------------------
+
+function togglePlay() {
+  // there is no 'play' property, only 'paused'
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+
+  /* ternary method with computed function name
+  const method = video.paused ? 'play' : 'pause';
+  // computed property method  
+  video[method]();
+  */
+}
+
+// separating this function from body of togglePlay
+function updateButton() {
+  // bound to video element
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  // console.log(this.dataset);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  // computed property name
+  video[this.name] = this.value
+}
+
+handle
+
+/* ------------------------
+  hook up the event listeners
+/* ------------------------
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
